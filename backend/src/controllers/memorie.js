3ee36609c5888ac3d933c33039099eeb3cd9e23e
gabrieldasilvadev <gabrieldasilvaprivado1@gmail.com>
2021-12-17 01:58:30 -0300

    Finaliza fluxo de autenticacao

--- backend/src/controllers/memorie.js
+++ backend/src/controllers/memorie.js
@@ -1,7 +1,15 @@
-import { authenticate as authService } from '../services/auth.js';
+import { authenticated } from '../services/auth.js';
 
 const memorie = (req, res) => {
-  return res.status(200).json({ message: 'sucesso' });
+  try {
+    authenticated(req.headers);
+
+    return res.status(200).json({ message: 'sucesso' });
+  } catch (error) {
+    return res
+      .status(error.status || 500)
+      .json({ message: error.message || 'Internal Error' });
+  }
 };
 
 export { memorie };
