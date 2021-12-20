345f636d27b7ab28b29117cf08e6a3fc7416f761
gabrieldasilvadev <gabrieldasilvaprivado1@gmail.com>
2021-12-20 06:37:06 -0300

    Configuracao do eslint e prettier formatacao do codigo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      h1
-    </div>
+    <Router>
+      <Router>
+        <Switch>
+          <Route path="/">
+            
+          </Route>
+          <Route path="/login">
+
+          </Route>
+          <Route path="/register">
+    
+          </Route>
+        </Switch>
+      </Router>
+    </Router>
   );
 }
 
